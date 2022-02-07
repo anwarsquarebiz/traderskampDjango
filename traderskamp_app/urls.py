0a07f5da1d663b3b76f3ff9d60184f226f277884
viddesh28 <99191448+viddesh28@users.noreply.github.com>
2022-02-07 19:37:31 +0530

    Update urls.py
    Added new urls from traderskamp.com also made new functions in views.py

--- traderskamp_app/urls.py
+++ traderskamp_app/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('signin', views.signIn, name='signin'),
+    path('signout', views.signOut, name='signout'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('register', views.register, name='register'),
+    path('resetpassword', views.resetpassword, name='resetpassword'),
 ]
